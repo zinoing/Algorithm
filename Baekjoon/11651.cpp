7d69aecbefd17e1e75f505bb7c2d3153a7f8274e
zinoing <77779979+zinoing@users.noreply.github.com>
2023-07-22 21:42:26 +0900

    Update 11651.cpp

--- Baekjoon/11651.cpp
+++ Baekjoon/11651.cpp
@@ -1,3 +1,4 @@
+// 버블 정렬로 구현
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
@@ -96,5 +97,122 @@ int main(void) {
     locations.sort();
     locations.show();
 
+    return 0;
+}
+
+// quick sort로 구현
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+class Location {
+    typedef struct Pos {
+        int xPos;
+        int yPos;
+
+        Pos operator=(const Pos& pos) {
+            xPos = pos.xPos;
+            yPos = pos.yPos;
+            return *this;
+        }
+
+    }Pos;
+    Pos* array;
+    int size;
+
+public:
+    Location(int n = 0)
+        :array(new Pos[n]), size(n) {}
+
+    Pos* getArray() {
+        return array;
+    }
+
+    int getxPos(int idx) {
+        return array[idx].xPos;
+    }
+
+    int getyPos(int idx) {
+        return array[idx].yPos;
+    }
+
+    Pos getPos(int idx) {
+        return array[idx];
+    }
+
+    void setPos(int idx, int xPos, int yPos) {
+        array[idx].xPos = xPos;
+        array[idx].yPos = yPos;
+        return;
+    }
+
+    void swap(Pos& a, Pos& b) {
+        Pos temp;
+        temp = a;
+        a = b;
+        b = temp;
+        return;
+    }
+
+    void quickSort(Pos* array, int head, int tail) {
+        if (head >= tail)
+            return;
+
+        Pos pivot = array[tail];
+        int i = head - 1;
+        for (int j = head; j <= tail; j++) {
+
+            if (j == tail) {
+                i++;
+                swap(array[j], array[i]);
+
+                quickSort(array, head, i-1);
+                quickSort(array, i + 1, tail);
+                return;
+            }
+
+
+            if (array[j].yPos > pivot.yPos)
+                continue;
+            else if (array[j].yPos < pivot.yPos) {
+                i++;
+                swap(array[j], array[i]);
+            }
+            else { // if two yPoses are same
+                if (array[j].xPos > pivot.xPos)
+                    continue;
+                else {
+                    i++;
+                    swap(array[j], array[i]);
+                }
+            }
+        }
+        return;
+    }
+
+    void show() {
+        for (int i = 0; i < size; i++) {
+            printf("%d %d \n", array[i].xPos, array[i].yPos);
+        }
+    }
+
+    ~Location() {
+        delete[] array;
+    }
+};
+
+int main(void) {
+    int n = 0;
+    scanf("%d", &n);
+
+    Location locations(n);
+
+    for (int i = 0; i < n; i++) {
+        int xPos, yPos;
+        scanf("%d %d", &xPos, &yPos);
+        locations.setPos(i, xPos, yPos);
+    }
+    locations.quickSort(locations.getArray(), 0, n-1);
+    locations.show();
     return 0;
 }
